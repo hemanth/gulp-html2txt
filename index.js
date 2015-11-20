91a132466af2e42b9207e3c5cda58e2386b7838b
javiercf <javiercferrans@gmail.com>
2015-11-20 02:46:33 -0500

    add all options
    parse all options available from html—to-txt

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ var es = require('event-stream');;
 var gutil = require('gulp-util');
 var html2txt = require('html-to-text');
 
-module.exports = function(wrap) {
-  wrap = wrap || 130
+module.exports = function(options) {
+  options = options || {}
+  
   return es.map(function (file, cb) {
-    file.contents = new Buffer( html2txt.fromString( file.contents.toString(), {
-      wordwrap: wrap
-    }) );
+    file.contents = new Buffer( html2txt.fromString( file.contents.toString(), options) );
     file.path = gutil.replaceExtension(file.path, '.txt');
     cb(null,file);
   });
